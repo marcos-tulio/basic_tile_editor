e34356d25666ea4ebf0b38a374cf1a40632abdba
Marcos Santos <marcos.ts201128@gmail.com>
2021-01-08 23:03:41 -0300

    corrigido bug ao iniciar o Frame

--- src/main/java/mrcapybara/control/Main.java
+++ src/main/java/mrcapybara/control/Main.java
@@ -3,6 +3,7 @@ package mrcapybara.control;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.lang.reflect.InvocationTargetException;
+import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import mrcapybara.control.JFrame.CtrlJFLanguage;
 import mrcapybara.control.JFrame.CtrlJFMain;
@@ -19,9 +20,11 @@ public class Main {
             sLanguage.setVisible(true);
             sLanguage.addWindowListener(new WindowAdapter() {
                 @Override
-                public void windowDeactivated(WindowEvent e) {
-                    super.windowDeactivated(e);
-                    new CtrlJFMain().setVisible(true);
+                public void windowClosed(WindowEvent e) {
+                    super.windowClosed(e);
+
+                    if (!sLanguage.isActive())
+                        new CtrlJFMain().setVisible(true);
                 }
             });
         });
